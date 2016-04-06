1c97e4c59d8f3c564c4a2e1aef5152f85bdb3207
Divyansh Prakash <divyansh@helpshift.com>
2016-04-06 19:26:14 +0530

    Refactored, updated README.

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Difference Engine
 
 Charles Babbage's [Difference Engine](https://en.wikipedia.org/wiki/Difference_engine) for Clojure.
+An alternative to `range`, `infer` returns a lazy sequence of zeroes of the least degree polynomial function inferred from pattern.
+
+[Here's](http://stackoverflow.com/questions/21503344/implementing-sequence-inference-in-clojure-using-method-of-differences) a short history of its evolution.
+
 
 ## Usage
 
 ```clojure
-(require '[difference-engine :refer :all])
+(require '[difference-engine :refer [infer]])
 
 
 (infer [1 2]) ;=> (1 2 3 4 5 ...)
@@ -15,7 +19,7 @@ Charles Babbage's [Difference Engine](https://en.wikipedia.org/wiki/Difference_e
 (infer [1 4 9]) ;=> (1 4 9 16 25 ...)
 
 
-(infer-n 5 [20 15]) ;=> (20 15 10 5 0)
+(infer 5 [20 15]) ;=> (20 15 10 5 0)
 ```
 
 ## License
--- src/difference_engine/core.clj
+++ src/difference_engine/core.clj
@@ -1,30 +1,36 @@
 (ns difference-engine.core)
 
 (defn ^:private diffs
-  "(diffs [1 4 9]) => (3 5)"
+  "Returns a lazy sequence of partial differences of coll.
+
+   (diffs [1 4 9]) => (3 5)"
   [coll]
   (map - (rest coll) coll))
 
 
 (defn ^:private triangle
-  "(triangle [1 4 9]) => ((1 4 9) (3 5) (2))"
+  "Returns a lazy sequence of subsequent partial
+   differences of pattern.
+
+   (triangle [1 4 9]) => ((1 4 9) (3 5) (2))"
   [pattern]
   (take (count pattern)
         (iterate diffs pattern)))
 
 
 (defn infer
-  "(infer [1 4 9]) => (1 4 9 16 25 ...)"
-  [pattern]
-  (->> (triangle pattern)
-       (map last) reverse
-       (drop-while zero?)
-       (iterate #(reductions + %))
-       (map last) rest
-       (concat pattern)))
+  "Returns a lazy sequence of zeroes of the least
+   degree polynomial function inferred from pattern.
+   To infinity by default, n if supplied.
 
-
-(defn infer-n
-  "(infer-n 5 [1 4 9]) => (1 4 9 16 25)"
-  [n pattern]
-  (take n (infer pattern)))
+   (infer [1 4 9]) => (1 4 9 16 25 ...)
+   (infer 5 [1 4 9]) => (1 4 9 16 25)"
+  ([pattern]
+   (->> (triangle pattern)
+        (map last) reverse
+        (drop-while zero?)
+        (iterate #(reductions + %))
+        (map last) rest
+        (concat pattern)))
+  ([n pattern]
+   (take n (infer pattern))))
