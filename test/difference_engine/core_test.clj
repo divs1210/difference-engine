6ed9d1c9ca7f003fc44c12d85e531dc5fccd2732
Divyansh Prakash <divyansh@helpshift.com>
2016-04-17 15:26:53 +0530

    Add tests

--- test/difference_engine/core_test.clj
+++ test/difference_engine/core_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [difference-engine.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest all-tests
+  (testing "+ve ints"
+    (is (= (infer 10 [1 2])
+           (range 1 11))))
+
+  (testing "-ve ints"
+    (is (= (infer 10 [-1 -2])
+           (range -1 -11 -1))))
+
+  (testing "ratios"
+    (is (= (infer 5 [1/8 2/8])
+           [1/8 2/8 3/8 4/8 5/8])))
+
+  (testing "doubles"
+    (is (= (infer 4 [0 0.25])
+           (range 0 1 0.25))))
+
+  (testing "squares"
+    (is (= (infer 10 [1 4 9])
+           (map #(* % %) (range 1 11))))))
